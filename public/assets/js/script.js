f2836180ffaa9b5b9a34bdc171d6b7df54b8ca2e
Jason Park <jihoonswdev@gmail.com>
2021-10-14 13:11:28 -0400

    about me

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,11 +6,23 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky");
         }
+        
     });
-});
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
+
+
+
+
+
+
+
+
+
+
+});
 
